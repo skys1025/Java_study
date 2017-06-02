43615ad232e38aa0ea259943ed0c5b2eadb4727d
skys1025 <skys1025@gmail.com>
2017-06-02 18:39:16 +0900

    implement Main class

--- src/study/Main.java
+++ src/study/Main.java
@@ -1,8 +1,31 @@
 package study;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+/**
+ * Invoke the main method of other class in study package.
+ */
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        try {
+            Class<?> c = Class.forName("study." + args[0]);
+            Class[] argTypes = new Class[]{String[].class};
+            Method main = c.getDeclaredMethod("main", argTypes);
+            String[] mainArgs = Arrays.copyOfRange(args, 1, args.length);
+            System.out.format("invoking %s.main()%n", c.getName());
+            main.invoke(null, (Object) mainArgs);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Class " + args[0] + " is not implemented.");
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 }
